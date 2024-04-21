6b4c0584083783a8153c055846d906e61d094c6d
Andrew Wright <agwright360@gmail.com>
2024-04-21 18:41:56 +0000

    docs: add Product model

--- products/models.py
+++ products/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
-    friendy_name = models.CharField(max_length=254, null=True, black=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -12,3 +16,22 @@ class Category(models.Model):
         return self.friendly_name
 
 
+class Product(models.Model):
+    RATING_CHOICES = (
+        (1, '1 Star'),
+        (2, '2 Stars'),
+        (3, '3 Stars'),
+        (4, '4 Stars'),
+        (5, '5 Stars'),
+    )
+
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    ingredients = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name

1118ada70c0c69ac7ffb6013c87611dac3aaa807
Andrew Wright <agwright360@gmail.com>
2024-04-28 11:59:37 +0000

    test: add test_views for product_details

--- products/test_views.py
+++ products/test_views.py
@@ -1,10 +1,43 @@
 from django.test import TestCase
+from .models import Range, Product
 
 
 class TestProductsViews(TestCase):
     """ Test products view """
 
+    def setUp(self):
+        """ Creates test objects for Products app """
+
+        self.range_test = Range.objects.create(
+            name="test_range",
+            friendly_name="Test Range",
+        )
+
+        self.product_test = Product.objects.create(
+            range=self.range_test,
+            name="Test Product",
+            category="Test Category",
+            description="Test Description",
+            ingredients="Test Ingredients",
+            price=9.99,
+            size="Test Size",
+            rating=4,
+            image="test_image.jpg",
+        )
+
     def test_products_page(self):
+        """ Test products url status code """
         response = self.client.get('/products/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/products.html')
+    
+    def test_products_details_page_with_valid_product(self):
+        """ Test product details url status code with valid product id """
+        response = self.client.get('/products/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/product_detail.html')
+    
+    def test_products_details_page_with_invalid_product(self):
+        """ Test product details url status code with invalid product id """
+        response = self.client.get('/products/2/')
+        self.assertEqual(response.status_code, 404)

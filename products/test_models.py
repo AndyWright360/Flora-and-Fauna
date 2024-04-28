df23e9a57527736062730399ff4e6c518c7c35c9
Andrew Wright <agwright360@gmail.com>
2024-04-28 11:39:56 +0000

    test: add products test_models

---
+++ products/test_models.py
@@ -0,0 +1,76 @@
+from django.test import TestCase
+from .models import Range, Product
+
+class TestProductsModels(TestCase):
+
+    def setUp(self):
+        """ Creates test objects for Products app """
+
+        self.range_test = Range.objects.create(
+            name="test_range",
+            friendly_name="Test Range",
+        )
+
+        self.product_test = Product.objects.create(
+            range=self.range_test,
+            name="Test Product",
+            category="Test Category",
+            description="Test Description",
+            ingredients="Test Ingredients",
+            price=9.99,
+            size="Test Size",
+            rating=4,
+            image="test_image.jpg",
+        )
+
+    def test_range_string_method(self):
+        """ Tests the string method on the range model """
+        self.assertEqual(str(self.range_test), self.range_test.name)
+
+    def test_get_friendly_name_method(self):
+        """ Test get_friendly_name() method on the range model """
+        self.assertEqual(str(self.range_test.get_friendly_name()), self.range_test.friendly_name)
+
+    def test_product_string_method(self):
+        """ Tests the string method on the product model """
+        self.assertEqual(str(self.product_test), self.product_test.name)
+    
+    def test_range_name(self):
+        """ Test the range name """
+        self.assertEqual(self.range_test.name, 'test_range')
+    
+    def test_range_friendly_name(self):
+        """ Test the range friendly name """
+        self.assertEqual(self.range_test.friendly_name, 'Test Range')
+    
+    def test_product_name(self):
+        """ Test the product name """
+        self.assertEqual(self.product_test.name, 'Test Product')
+
+    def test_product_category(self):
+        """ Test the product category """
+        self.assertEqual(self.product_test.category, 'Test Category')
+
+    def test_product_description(self):
+        """ Test the product description """
+        self.assertEqual(self.product_test.description,'Test Description')
+    
+    def test_product_ingredients(self):
+        """ Test the product ingredients """
+        self.assertEqual(self.product_test.ingredients,'Test Ingredients')
+    
+    def test_product_price(self):
+        """ Test the product price """
+        self.assertEqual(self.product_test.price, 9.99)
+
+    def test_product_size(self):
+        """ Test the product size """
+        self.assertEqual(self.product_test.size, 'Test Size')
+
+    def test_product_rating(self):
+        """ Test the product rating """
+        self.assertEqual(self.product_test.rating, 4)
+
+    def test_product_image(self):
+        """ Test the product image """
+        self.assertEqual(self.product_test.image, 'test_image.jpg')

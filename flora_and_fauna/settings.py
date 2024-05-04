db1f48a6d3d23d8017b6728bb973bcc2b07ba344
Andrew Wright <agwright360@gmail.com>
2024-05-04 16:29:44 +0000

    docs: create reviews app and models

--- flora_and_fauna/settings.py
+++ flora_and_fauna/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'reviews',
 
     # Other
     'crispy_forms',
---
+++ reviews/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from products.models import Product
+
+
+class Review(models.Model):
+    RATING_CHOICES = (
+        (1, '1 Star'),
+        (2, '2 Stars'),
+        (3, '3 Stars'),
+        (4, '4 Stars'),
+        (5, '5 Stars'),
+    )
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True, related_name="reviews",)
+    created_on = models.DateField(auto_now_add=True, blank=False, null=False)
+    title = models.CharField(max_length=50, blank=False, null=False)
+    content = models.TextField(max_length=500)
+    rating = models.IntegerField(choices=RATING_CHOICES, null=False)
+
+    def __str__(self):
+        return self.title

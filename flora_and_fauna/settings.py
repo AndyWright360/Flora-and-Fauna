21b304e29008f672c0a0b9bb6479643cc485568f
Andrew Wright <agwright360@gmail.com>
2024-05-05 11:02:18 +0000

    docs: create profiles app and models

--- flora_and_fauna/settings.py
+++ flora_and_fauna/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'reviews',
+    'profiles',
 
     # Other
     'crispy_forms',
--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Review(models.Model):
@@ -11,7 +12,9 @@ class Review(models.Model):
         (5, '5 Stars'),
     )
 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True, related_name="reviews",)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True, related_name='reviews',)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='reviews')
     created_on = models.DateField(auto_now_add=True, blank=False, null=False)
     title = models.CharField(max_length=50, blank=False, null=False)
     content = models.TextField(max_length=500)

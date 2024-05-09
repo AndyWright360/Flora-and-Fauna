6caef64db5a97f17433d92aaee76bea598260ed3
Andrew Wright <agwright360@gmail.com>
2024-05-09 19:59:15 +0000

    test: add test_forms for reviews

---
+++ reviews/test_forms.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from .forms import ReviewForm
+from .models import Review
+
+
+class TestReviewForm(TestCase):
+
+    def test_review_title_required(self):
+        """ 
+        Tests the title field in the review form is required.
+        """
+        form = ReviewForm({'title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    def test_review_content_required(self):
+        """ 
+        Tests the content field in the review form is required.
+        """
+        form = ReviewForm({'content': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('content', form.errors.keys())
+        self.assertEqual(form.errors['content'][0], 'This field is required.')
+
+    def test_review_rating_required(self):
+        """ 
+        Tests the rating field in the review form is required.
+        """
+        form = ReviewForm({'rating': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('rating', form.errors.keys())
+        self.assertEqual(form.errors['rating'][0], 'This field is required.')
+

f1a563154438676c307ba279631926288ce32467
Andrew Wright <agwright360@gmail.com>
2024-05-10 21:38:04 +0000

    docs: add wishlist details to admin

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WishlistItem
 
-# Register your models here.
+
+class WishlistAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'product', 'date_added',)
+
+admin.site.register(WishlistItem, WishlistAdmin)

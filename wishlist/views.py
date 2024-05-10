0f64b62a59a5fbf91bb5275025578b995027fe98
Andrew Wright <agwright360@gmail.com>
2024-05-10 21:26:00 +0000

    feat: implement add item to wishlist functionality

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, get_object_or_404
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import WishlistItem
+from products.models import Product
+
+@login_required()
+def add_to_wishlist(request):
+    product_id = request.GET['id']
+    product = Product.objects.get(id=product_id)
+
+    context = {}
+
+    wishlist_count = WishlistItem.objects.filter(product=product, user=request.user).count()
+    print(wishlist_count)
+
+    if wishlist_count > 0:
+        context = {
+            "bool": True
+        }
+    else:
+        new_wishlist = WishlistItem.objects.create(
+            product=product,
+            user=request.user
+        )
+        context = {
+                "bool": True
+            }
+    
+    return JsonResponse(context)
+
